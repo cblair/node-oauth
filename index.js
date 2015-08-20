12aeba28f7ad31d2df760422b829fedb63151a57
Colby Blair <cblair@vandals.uidaho.edu>
2015-08-20 12:31:06 -0700

    updating main file to export for AMD or CommonJS.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-exports.OAuth = require("./lib/oauth").OAuth;
-exports.OAuthEcho = require("./lib/oauth").OAuthEcho;
-exports.OAuth2 = require("./lib/oauth2").OAuth2;
+(function () {
+    "use strict";
+
+    // EXPORTS
+
+    // AMD
+    if ( typeof define == 'function' && define.amd ) {
+        define(["oauthlib/oauth", "oauthlib/oauth", "oauthlib/oauth2"],
+            function (OAuth, OAuthEcho, OAuth2) {
+
+                return {
+                    OAuth: OAuth,
+                    OAuthEcho: OAuthEcho,
+                    OAuth2: OAuth2
+                };
+        });
+
+    // Node and other environments that support module.exports.
+    } else if ( typeof module != 'undefined' && module.exports ) {
+        exports.OAuth = require("./lib/oauth").OAuth;
+        exports.OAuthEcho = require("./lib/oauth").OAuthEcho;
+        exports.OAuth2 = require("./lib/oauth2").OAuth2;
+    }
+}(this));
